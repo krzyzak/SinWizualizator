028497b77fedc2dcec2b37e505ffd5bc77ceb1de
Michał Krzyżanowski <michal.krzyzanowski+github@gmail.com>
2010-12-05 10:54:33 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sinwizualizator::Application.config.secret_token = '694482d7b098d66b77a0dce6aa52f3fc4fc2cfd5f5245ddf1fa4cdf72671eb2422337644482b84a915e0cecff9fea066c7e9700dc18920e4ce7c8ac8d186bbf8'
