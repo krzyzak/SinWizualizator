5037a6fae8e9994553f22949a6437733ec40ce93
Michał Krzyżanowski <michal.krzyzanowski+github@gmail.com>
2010-12-05 11:32:16 +0100

    added necessary methods - find, find_or_create and path

--- app/models/image.rb
+++ app/models/image.rb
@@ -4,7 +4,8 @@ class Image
 
   attr_accessor :name
   attr_reader :exists
-
+  @@base_dir = "/test"
+  @ext = "png"
   validate do |image|
       image.errors.add_to_base("Name can't be blank") if image.name.blank?
     end
@@ -26,8 +27,26 @@ class Image
     [@name] if persisted?
   end
 
+  def path
+    "#{@base_dir}/#{self.name}.#{@ext}"
+  end
+
   def save
     #TODO: implement image saving
     @exists = true
+  end
+
+  def self.find(name)
+    "#{@@base_dir}/#{name}.#{@@ext}" if File.exists?("#{@@base_dir}/#{name}.#{@@ext}")
+  end
+
+  def self.find_or_create(name)
+    if File.exists?("#{@@base_dir}/#{name}.#{@@ext}")
+      "#{@@base_dir}/#{name}.#{@@ext}"
+    else
+      @image = Image.new(name)
+      @image.save
+      @image.path
+    end
   end
 end
