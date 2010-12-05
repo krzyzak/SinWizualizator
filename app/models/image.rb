c0439c3440b0cd2c67306aef0423d5d89d733384
Michał Krzyżanowski <michal.krzyzanowski+github@gmail.com>
2010-12-05 11:19:38 +0100

    added base methods - it passes basic test now.

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,5 +1,33 @@
 class Image
+  include ActiveModel::Validations
   extend ActiveModel::Naming
 
+  attr_accessor :name
+  attr_reader :exists
 
+  validate do |image|
+      image.errors.add_to_base("Name can't be blank") if image.name.blank?
+    end
+
+  def initialize(name = nil)
+    @name = name
+    @exists = false
+  end
+
+  def persisted?
+    @exists
+  end
+
+  def to_param
+    @name if persisted?
+  end
+
+  def to_key
+    [@name] if persisted?
+  end
+
+  def save
+    #TODO: implement image saving
+    @exists = true
+  end
 end
